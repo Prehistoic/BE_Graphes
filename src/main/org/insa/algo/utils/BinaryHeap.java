f48e540ff51016cf5a5dcdb532a561deb5009830
LACOTE Matthieu <mat.lac702@gmail.com>
2019-04-04 12:11:46 +0200

    debug binary heap

--- src/main/org/insa/algo/utils/BinaryHeap.java
+++ src/main/org/insa/algo/utils/BinaryHeap.java
@@ -144,7 +144,26 @@ public class BinaryHeap<E extends Comparable<E>> implements PriorityQueue<E> {
 
     @Override
     public void remove(E x) throws ElementNotFoundException {
-        // TODO:
+        int index_last;
+        int index;
+    	if(this.isEmpty()){
+    		throw new ElementNotFoundException(x);
+    	}
+    	else {
+    		index = this.array.indexOf(x);
+    		if(index == -1 || index>=this.currentSize) {
+    			throw new ElementNotFoundException(x);
+    		}
+    		else {
+    			index_last = --this.currentSize;
+    	
+            	if(index_last > index) {
+            		this.arraySet(index, this.array.get(index_last));
+            		this.percolateDown(index);
+            		this.percolateUp(index);
+            	}
+    		}
+    	}
     }
 
     @Override
